fb33a29aae102396a7c9dffd912b7456fb9e61b1
Yuliya Bukhtik <yuliya.bukhtik@gmail.com>
2020-03-30 17:19:06 +0300

    Correction task02_checking the required format added

--- HW02/Yuliya_Bukhtik/yuliya_bukhtik_hw_02_t_02.rb
+++ HW02/Yuliya_Bukhtik/yuliya_bukhtik_hw_02_t_02.rb
@@ -1,24 +1,26 @@
 # frozen_string_literal: true
 
-file_path = 'log_file_task_02.txt'
+file_path = 'homeworks-2020/HW02/Yuliya_Bukhtik/yuliya_bukhtik_hw_02_t_02.rb'
 
 def task_2(file)
-  lines = File.readlines file
+  lines = File.readlines(file)
 
-  # by the below method lines with ERROR will be deleted
-  lines.delete_if { |line| line.include? 'error' }
+  requiered_format = Regexp.new %r{\d+\.\d+\.\d+\.\d+ - -
+    \[\d{1,2}\/[a-zA-Z]+\/\d{4}:\d{1,2}:\d{1,2}:\d{1,2} \+\d+] "
+    [A-Z]{4} \/[a-z]{4}\/\d\/[a-z]+ [A-Z]{4}\/1.1" .+}
 
-  # by the below method the rest lines will be parsed into the required format
-  lines.map! do |line|
-    parsing_new_line line
+  lines.select! do |line|
+    line.match(/#{requiered_format}/)
   end
 
-  f.close
+  # by the below method the rest lines will be parsed into the required format
+  lines.map! { |line| parsing_new_line(line) }
+
   puts lines
 end
 
 def parsing_new_line(line)
-  ip = Regexp.new '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
+  ip = Regexp.new '\d+\.\d+\.\d+\.\d+'
   date = Regexp.new '\d{1,2}\/[a-zA-Z]+\/\d{4}:\d{1,2}:\d{1,2}:\d{1,2} \+\d+'
 
   # getting POST address in upcase
@@ -29,4 +31,4 @@ def parsing_new_line(line)
   "#{line[date]} FROM: #{line[ip]} TO: #{address_format}"
 end
 
-task_2 file_path
+task_2(file_path)
