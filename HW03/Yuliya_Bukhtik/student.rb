57f6d31c20e60294dfa806f4f6092967a4915f90
Yuliya Bukhtik <yuliya.bukhtik@gmail.com>
2020-04-07 14:39:13 +0300

    Add module Observable in a new file

--- HW03/Yuliya_Bukhtik/student.rb
+++ HW03/Yuliya_Bukhtik/student.rb
@@ -1,26 +1,6 @@
 # frozen_string_literal: true
 
-# this module is a subject's interface
-
-module Observable
-  def initialize
-    @observers = []
-  end
-
-  def add_observer(observer)
-    @observers << observer unless @observers.include?(observer)
-  end
-
-  def delete_observer(observer)
-    @observers.delete(observer)
-  end
-
-  def notify_observers
-    @observers.each { |x| x.update(self) }
-  end
-end
-
-# this class is Student
+require 'observable'
 
 class Student
   include Observable
