a191761d977d4260be37c97effb750a00397fd77
Yuliya Bukhtik <yuliya.bukhtik@gmail.com>
2020-04-20 18:12:34 +0300

    Rubocop bag fix

--- HW04/Yuliya_Bukhtik/my_module.rb
+++ HW04/Yuliya_Bukhtik/my_module.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 module MyModule
-  def my_each(&block)
-    self.length.times do |item|
+  def my_each
+    length.times do |item|
       yield(self[item])
     end
     self
   end
 
-  def my_select(&block)
+  def my_select
     new_array = []
 
     for item in self do
@@ -17,7 +17,7 @@ module MyModule
     new_array
   end
 
-  def my_map(&block)
+  def my_map
     new_array = []
 
     for item in self do
